d4d12c8246485e1128f6c2bac62760a63a3b5772
newoc1 <owencapo1@gmail.com>
2016-11-08 21:37:45 -0500

    entity for car. The geolocation should be a value object, but still need to persist it. Since new geolocations will be assigned to a car fairly frequently, will want to be careful with selection of database tool.

--- crs/src/main/java/com/grok/crs/Car.java
+++ crs/src/main/java/com/grok/crs/Car.java
@@ -1,5 +1,30 @@
 package com.grok.crs;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Car {
 
+	@Column
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	@Column
+	private String name;
+	
+	@Column
+	private int payGrade;
+	
+	public Car(){	
+	}
+	
+	public Car(String name, int payGrade){
+		this.name = name;
+		this.payGrade = payGrade;
+	}
+	
 }
--- crs/src/main/java/com/grok/crs/Geolocation.java
+++ crs/src/main/java/com/grok/crs/Geolocation.java
@@ -1,5 +1,8 @@
 package com.grok.crs;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
 public class Geolocation {
 
 	private double latitude;
@@ -17,43 +20,29 @@ public class Geolocation {
 	public double getLatitude() {
 		return latitude;
 	}
-	public void setLatitude(double latitude) {
-		this.latitude = latitude;
-	}
+
 	public double getLongitude() {
 		return longitude;
 	}
-	public void setLongitude(double longitude) {
-		this.longitude = longitude;
-	}
+
 	public double getAltitude() {
 		return altitude;
 	}
-	public void setAltitude(double altitude) {
-		this.altitude = altitude;
-	}
+
 	public double getAccuracy() {
 		return accuracy;
 	}
-	public void setAccuracy(double accuracy) {
-		this.accuracy = accuracy;
-	}
+
 	public double getAltitudeAccuracy() {
 		return altitudeAccuracy;
 	}
-	public void setAltitudeAccuracy(double altitudeAccuracy) {
-		this.altitudeAccuracy = altitudeAccuracy;
-	}
+
 	public double getHeading() {
 		return heading;
 	}
-	public void setHeading(double heading) {
-		this.heading = heading;
-	}
+
 	public double getSpeed() {
 		return speed;
 	}
-	public void setSpeed(double speed) {
-		this.speed = speed;
-	}
+
 }
